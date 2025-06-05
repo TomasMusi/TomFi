80faef6e73dc97354b0d29435e993e8763a28b1a
TomasMusi <tomas.musilmt@gmail.com>
2025-06-05 17:04:25 +0200

    Pin Notification

--- src/lib/alerts.ts
+++ src/lib/alerts.ts
@@ -16,10 +16,55 @@ export const showSucess = (message: string) => {
     Swal.fire({
         position: 'top-end',
         icon: 'success',
-        title: 'Registrace Uspesna!',
+        title: message,
         showConfirmButton: false,
         timerProgressBar: true,
         toast: true,
         timer: 1500
     });
 }
+
+
+export const showInputAlert = (message: string, messageButton: string) => {
+    Swal.fire({
+        title: message,
+        input: "text",
+        inputAttributes: {
+            autocapitalize: "off"
+        },
+        showCancelButton: true,
+        confirmButtonText: messageButton,
+        showLoaderOnConfirm: true,
+        preConfirm: async (password: string) => {
+            try {
+                const response = await fetch('/api/register', {
+                    method: 'POST',
+                    body: JSON.stringify(password),
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                });
+                if (!response.ok) {
+                    return Swal.showValidationMessage(`
+          ${JSON.stringify(await response.json())}
+        `);
+                }
+                return response.json();
+            } catch (error) {
+                Swal.showValidationMessage(`
+        Failed: ${error}
+      `);
+            }
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: `${result.value.login}'s avatar`,
+                imageUrl: result.value.avatar_url
+            });
+        }
+    });
+}
+
+
